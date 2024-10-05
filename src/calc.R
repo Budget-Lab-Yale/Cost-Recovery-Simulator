b04063f5cf9d7d678e79ff81c65f7ab37e0d6028
John Ricco <john.ricco@yale.edu>
2024-10-04 21:22:15 -0400

    Add indexation calculation; speed up half-year shifting

--- src/calc.R
+++ src/calc.R
@@ -1,50 +1,71 @@
-calc_depreciation = function(expenses) {
+calc_depreciation = function(investment, macro_projections) {
   
   # Pulls number from schedule TODO
-  h = max(expenses$L)
+  h = max(investment$L)
   
   # All possible years a deduction can be taken
-  all_years = c(unique(expenses$year), (max(expenses$year)+1):(max(expenses$year) + h))
+  all_years = c(unique(investment$year), (max(investment$year)+1):(max(investment$year) + h))
+
+  # Parse indexation values
+  indexes = macro_projections %>% 
+    mutate(inflation = cpi / lag(cpi) - 1) %>% 
+    select(year, inflation, timevalue = yield)
   
-  1:nrow(expenses) %>%
-    map(.f = ~ calc_macrs(expenses[.x,], all_years)) %>%
+  1:nrow(investment) %>%
+    map(.f = ~ calc_macrs(investment[.x,], all_years)) %>%
     bind_rows() %>%
     return()
   
 }
 
-calc_macrs = function(expenses, all_years) {
+calc_macrs = function(investment, all_years) {
   
   # Recasts values to be more usuable
   
   # Year of investment
-  year = as.integer(expenses["year"])
+  year = as.integer(investment["year"])
   # Length of schedule
-  L = as.numeric(expenses["L"])
+  L = as.numeric(investment["L"])
   # Asset class parameter
-  B = as.numeric(expenses["B"])
+  B = as.numeric(investment["B"])
   # Section 179 parameter
-  s179 = as.numeric(expenses["s179"])
+  s179 = as.numeric(investment["s179"])
   
-  # Calculate balance to be expensed, after withrdawing bonus
-  balance = as.numeric(expenses["investment"])
-  bonus = balance * (s179 + as.numeric(expenses["bonus"]) * (1 - s179))
-  balance = balance - bonus
+  # Calculate balance to be depreciated after accounting for amount expensed
+  balance = as.numeric(investment["investment"])
+  expensed = balance * (s179 + as.numeric(investment["bonus"]) * (1 - s179))
+  balance = balance - expensed
   
+  # Calculate schedule of MACRS deductions
   deductions = apply_deduction(balance, B, L)
   
-  # Add in bonus and calculate for half year splits
-  deductions[1] = deductions[1] + bonus
-  out = deductions[1] / 2
+  # Add expensed amount to schedule 
+  deductions[1] = deductions[1] + expensed
   
-  for(i in 2:length(deductions)) {
-    out = c(out, deductions[i-1]/2 + deductions[i]/2)
+  # Account for half-year convention (assume all investment occurs in middle of the year)
+  out = (deductions / 2) + lag(deductions / 2, default = 0)
+  
+  # Adjust for inflation/time value if specified under law
+  if (investment$indexation %in% c('inflation', 'timevalue')) {
+    
+    # Calculate and apply path of indexation adjustment factors
+    index_years = year:(year + length(out) - 1)
+    index_adjustment = indexes %>% 
+      filter(year %in% index_years) %>% 
+      select(value = !!investment$indexation) %>% 
+      mutate(value = cumprod(1 + lag(value, default = 0))) %>% 
+      deframe()
+    out = out * index_adjustment
+    
+  } else {
+    if (investment$indexation != 'none') {
+      stop('Invalid tax law parameter value for indexation')
+    }
   }
   
-  L = ceiling(L)
-  
   # Set up and Construct output row
-  asset_class = rep(expenses["asset_class"], L+1)
+  L = ceiling(L)
+  asset_class = rep(investment["asset_class"], L+1)
   years = year:(year+L)
   year = rep(year, L+1)
   out = c(out, deductions[L]/2)
@@ -140,70 +161,5 @@ calc_npv = function(df) {
     return()
   
 }
-
-
-
-
-# calc_depreciation = function(expenses, year, years, revenue = T, tsy = NULL) {
-#   df = expenses %>%
-#     pivot_longer(!year, names_to = 'schedule', values_to = 'investment') %>%
-#     left_join(., rate_schedule) %>%
-#     mutate(
-#       bonus = bonus * investment,
-#       investment = investment - bonus,
-#       across(starts_with('V'),
-#              ~ .x * investment),
-#       V1 = V1 + bonus
-#     ) %>%
-#     select(!bonus)
-#   
-#   if(revenue) {
-#     df %>%
-#       rename_with(~as.character(year - 1 + as.integer(str_sub(., 2))), .cols = starts_with('V')) %>%
-#       fill_years(., years) %>%
-#       pivot_longer(!c(year, schedule, investment), names_to = 'd_year', values_to = 'depreciation') %>%
-#       arrange(d_year) %>%
-#       filter(d_year %in% as.integer(years)) %>%
-#       pivot_wider(names_from = d_year, values_from = depreciation) %>%
-#       return()
-#     
-#   } else {
-#     df %>%
-#       rename_with(~ paste0('t+', as.integer(str_sub(., 2)) - 1), .cols = starts_with('V')) %>%
-#       return()
-#   }
-#   
-# }
-
-
-# npv = depreciation_npv %>% filter(year == 2025) %>%
-#   pivot_longer(!c(year, schedule, investment), names_to = 'period', values_to = 'depreciation') %>%
-#   left_join(., tsy) %>%
-#   mutate(
-#     npv = depreciation / (1 + tsy_10y + spread)^(as.integer(str_sub(period, -1)))
-#   ) %>%
-#   select(year, schedule, period, npv) %>%
-#   pivot_wider(names_from = period, values_from = npv) %>%
-#   mutate(
-#     npv = rowSums(across(starts_with('t+')))
-#   ) %>%
-#   select(!contains('t+'))
-# 
-# 
-# 
-# tsy = 'projections.csv' %>%
-#   map(.f = ~ file.path(interface_paths$`Macro-Projections`, .x) %>%
-#         read_csv()
-#   ) %>%
-#   bind_rows() %>%
-#   filter(between(year, 2025, 2025 + 20)) %>%
-#   select(year, tsy_10y) %>%
-#   mutate(
-#     tsy_10y = (tsy_10y / 100) + 1, 
-#     spread = .02,
-#     period = paste0('t+', year - 2025)
-#   ) %>%
-#   select(!year)
-
 
 
